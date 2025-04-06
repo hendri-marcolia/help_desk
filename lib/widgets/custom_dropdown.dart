03d52b11dcc3db6eb2dbd08b87f09ca2317b206e
Hendri Marcolia <hendri.marcolia@gmail.com>
2025-04-07 00:13:34 +0700

    Fix Error on UI load

--- lib/widgets/custom_dropdown.dart
+++ lib/widgets/custom_dropdown.dart
@@ -17,7 +17,7 @@ class CustomDropdown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DropdownButtonFormField<String>(
-      value: value ?? 'All',
+      value: value,
       decoration: InputDecoration(
         hintText: hint,
         border: OutlineInputBorder(
